9e4a329012921fbd3efa82924857e988c2fd8d61
Cynthia Gamarra <cgamarra@fi.uba.ar>
2019-04-17 19:01:07 -0300

    Agrego modificaciones del ejer 3

--- TPEj3.py
+++ TPEj3.py
@@ -1,24 +1,18 @@
 #/usr/bin/env/ python
-import math 
 import numpy as np
 import matplotlib.pyplot as plt
-
-
-
-###Box muller
-
-def z1(u1,u2):
-	return math.sqrt(-2 * math.log(u1)) * math.cos(u2)
-
-def z2(u1,u2):
-	return math.sqrt(-2 * math.log(u1)) * math.sen(u2)
 
 # distribucion normal
 u1 = np.random.normal(0,1, 100000)
 u2 = np.random.normal(0,1, 100000)
 
+###Box muller
+z1 = np.sqrt(-2 * np.log(u1)) * np.cos(u2)
+z2 = np.sqrt(-2 * np.log(u1)) * np.sen(u2)
 
+#Histograma
 plt.hist(u1, bins =60, alpha=0.5, ec='black')
+plt.hist(u2, bins =60, alpha=0.5, ec='green')
 plt.grid(True)
 plt.show()
 

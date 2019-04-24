87987117676c8b9c3af8d4dd1487ffe46b5dafa2
malonso <malonso@practia.global>
2019-04-24 19:19:14 -0300

    ejercicio 7 y graficos

--- ejercicios/TPEj7.py
+++ ejercicios/TPEj7.py
@@ -1,7 +1,13 @@
 #/usr/bin/env/ python
 # -*- coding: utf-8 -*-
 
+import math 
+import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.colors
+from matplotlib.pyplot import plot
+from mpl_toolkits.mplot3d import axes3d
+
 
 modulo = 2**32
 multiplicador = 1013904223
@@ -20,9 +26,29 @@ def cargarSecuencia(secuencia,inicio, fin):
 #Cargo la lista de secuencias
 cargarSecuencia(secuencia,1, 100000)
 
-#Gráfico en dos dimensiones
-plt.specgram(secuencia, NFFT=256, Fs=2, Fc=0,noverlap=128)
+x=[]
+y=[]
+z=[]
+k=0
 
-#Gráfico en tres dimensiones
+for i in (0,len(secuencia)-3):
+	x.append(secuencia[i+k])
+	k+1
+	y.append(secuencia[i+k])
+	k+1
+	z.append(secuencia[i+k])
+	k+1
 
-plt.show()
+#grafico 3d
+fig=plt.figure()
+ax=fig.gca(projection='3d')
+ax.plot(x,y,z,linewidth=0.5)
+plt.show()	
+
+#grafico 2d
+fig=plt.figure()
+plt.xlim(1,10000000)
+plt.ylim(1,10000000)
+plot(x,y,linewidth=0.5, markersize=12)
+plt.show()	
+

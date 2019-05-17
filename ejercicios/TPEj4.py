c71b8a596b7b7d0e4f6152b82c38aa0ec8c07b09
tomaspinto <tomasp834@gmail.com>
2019-05-17 02:06:54 -0300

    fix ej4

--- ejercicios/TPEj4.py
+++ ejercicios/TPEj4.py
@@ -1,25 +1,41 @@
 import numpy as np
 import scipy.stats as st
 import matplotlib.pyplot as plt
+import math
 
 def p(x):
+	#target
     return st.norm.pdf(x, loc=40, scale=6)
 
 
 def q(x):
-    return st.norm.pdf(x, loc=50, scale= 20)
+	#proposed
+	y = np.array([])
+	for number in x:
+		n = abs(number)
+		lambd = 0.01
+		res = (math.e**(-n * lambd)) * lambd
+		y = np.append(y, res)
+	return y
 
+def q2(n):
+	#proposed
+	#misma funcion que q(x) pero como la tuve que implementar a mano, la funcion anterior
+	#no funcionaba si tenia que recibir un solo numero, como se hace en rejection_sampling
+	lambd = 0.01
+	res = (math.e**(-n * lambd)) * lambd
+	return res
 
 x = np.arange(-50, 151)
 k = max(p(x) / q(x))
-
 
 def rejection_sampling(iter=1000):
     samples = []
 
     for i in range(iter):
-        z = np.random.normal(50, 20)
-        u = np.random.uniform(0, k*q(z))
+        z = np.random.exponential(float(1)/0.01) #aca es 1/lambda porque recibe la media
+
+        u = np.random.uniform(0, k*q2(z))
 
         if u <= p(z):
             samples.append(z)

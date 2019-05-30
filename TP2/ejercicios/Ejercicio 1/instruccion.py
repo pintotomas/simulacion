a87c5360dcf262a463446142d553f95d0df7ad18
tomaspinto <tomasp834@gmail.com>
2019-05-30 15:36:21 -0300

    Instruccion

---
+++ TP2/ejercicios/Ejercicio 1/instruccion.py
@@ -0,0 +1,34 @@
+import random
+
+class InstruccionFactory(object):
+  
+  @staticmethod
+  def nueva_instruccion():
+    # Tiro un random entre 0 y 1, si es menor o igual a 0.6 entonces
+    # es una instruccion simple 
+    # idem memoria
+    tipo = random.uniform(0,1)
+    memoria = random.uniform(0,1)
+    lee_memoria = False
+    if (memoria < 0.65):
+      lee_memoria = True
+    if (tipo <= 0.6):
+      return InstruccionSimple(lee_memoria)
+    else:
+      return InstruccionCompleja(lee_memoria)
+
+class InstruccionCompleja(object):
+
+  def __init__(self, lee_memoria):
+    self.lee_memoria = lee_memoria
+
+  def costo(self):
+    return 10
+
+class InstruccionSimple(object):
+
+  def __init__(self, lee_memoria):
+    self.lee_memoria = lee_memoria
+
+  def costo(self):
+    return 60

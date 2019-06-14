41ff57b8c0b82896ee4c3d47c7f2b47140c512ce
Tomas <tomasp834@gmail.com>
2019-06-14 16:09:24 -0300

    Agrego un main para correr las dos distintas alternativas

--- TP2/ejercicios/Ejercicio 1/procesador.py
+++ TP2/ejercicios/Ejercicio 1/procesador.py
@@ -1,10 +1,11 @@
 import queue
 import time
 
-def procesar(cola_instrucciones):
+def procesar(cola_instrucciones, modo):
   cantidad_procesada = 0
   while True:
     if not (cola_instrucciones.empty()):
       time.sleep(cola_instrucciones.get().costo())
       cantidad_procesada += 1
-      print("\rCantidad procesada: "+str(cantidad_procesada), end="")
+      print("\r" + modo + " Cantidad procesada: "+str(cantidad_procesada), end="")
+      

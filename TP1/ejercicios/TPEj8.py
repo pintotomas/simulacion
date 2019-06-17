63fbc65838e89906124433ec7f7ebc1875f29f84
Cynthia Gamarra <cgamarra@fi.uba.ar>
2019-06-17 19:06:23 -0300

    Agrego esqueleto para el informe del tp2

--- TP1/ejercicios/TPEj8.py
+++ TP1/ejercicios/TPEj8.py
@@ -4,9 +4,9 @@ from scipy import stats as sp
 from numpy import random
 
 #Tomamos los resultados del ejercicio 5
-valoresObservados=[39852,30001,12106,9953,9004]
+valoresObservados=[39440.,30081.,12106.,10317.,8093.]
 #Tomamos los valores esperados por a distribución
-valoresEsperados=[40000,30000,12000,10000,8000]
+valoresEsperados=[40000.0, 30000.0, 12000.0, 10000.0, 8000.0]
 
 estadistica, pvalue = sp.chisquare(valoresObservados,valoresEsperados)
 print(estadistica)

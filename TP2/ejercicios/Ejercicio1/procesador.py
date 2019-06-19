f1dd89182e3d3ae80b471ef5b9a0437098964cf5
Tomas <tomasp834@gmail.com>
2019-06-19 17:49:13 -0300

    fix ejercicio1, ejerccio 4 casi terminado

--- TP2/ejercicios/Ejercicio1/procesador.py
+++ TP2/ejercicios/Ejercicio1/procesador.py
@@ -1,13 +1,14 @@
 import queue
 import time
 import csv
+import numpy
 def procesar(cola_instrucciones, modo):
   cantidad_procesada = 0
   minutos_procesamiento = 0
   start_time = time.time()
   while True:
     if not (cola_instrucciones.empty()):
-      time.sleep(cola_instrucciones.get().costo())
+      time.sleep(numpy.random.exponential(cola_instrucciones.get().costo()))
       cantidad_procesada += 1
       print("\r" + modo + " Cantidad procesada: "+str(cantidad_procesada), end="")
 
---
+++ TP2/ejercicios/Ejercicio4/instruccion.py
@@ -0,0 +1,35 @@
+import random
+SECOND_TO_MICROSECOND = 1000000
+class InstruccionFactory(object):
+  
+  @staticmethod
+  def nueva_instruccion():
+    # Tiro un random entre 0 y 1, si es menor o igual a 0.6 entonces
+    # es una instruccion simple 
+    # idem memoria
+    tipo = random.uniform(0,1)
+    memoria = random.uniform(0,1)
+    lee_memoria = False
+    if (memoria <= 0.65):
+      lee_memoria = True
+    if (tipo <= 0.6):
+      return InstruccionSimple(lee_memoria)
+    else:
+      return InstruccionCompleja(lee_memoria)
+
+class InstruccionCompleja(object):
+
+  def __init__(self, lee_memoria):
+    self.lee_memoria = lee_memoria
+
+  def costo(self):
+    return 10#/float(SECOND_TO_MICROSECOND)
+
+class InstruccionSimple(object):
+
+  def __init__(self, lee_memoria):
+    self.lee_memoria = lee_memoria
+
+  def costo(self):
+    return 60#/float(SECOND_TO_MICROSECOND)
+    

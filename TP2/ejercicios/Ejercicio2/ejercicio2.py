3caf8423349bcac03ab1f4a07e37e50f87dbe2c3
malonso <malonso@practia.global>
2019-06-21 21:10:16 -0300

    ejercicio 2 preliminar

--- TP2/ejercicios/Ejercicio2/ejercicio2.py
+++ TP2/ejercicios/Ejercicio2/ejercicio2.py
@@ -1,7 +1,9 @@
 import os
 import numpy
+import random
+import matplotlib.pyplot as plt
 
-cant=50
+cant=51
 p = 0.7
 
 def factorial(n):
@@ -19,30 +21,35 @@ def jMayorIgualAi(i,j):
 
 def jMenorAi(i,j):
 	sumatoria =0
-	for k in range(i,cant+1):
+	for k in range(i,cant):
 		 sumatoria = sumatoria + jMayorIgualAi(i,k)
-	return (1-sumatoria)
+	return (1-sumatoria)/i
 
 def funcionProbabilidad(i,j):
 	if (i<=j):
 		probabilidad = jMayorIgualAi(i,j)
 	else:
 		probabilidad = jMenorAi(i,j)
-	#return round(probabilidad,4)
-	return probabilidad
+	return round(probabilidad,4)
 
 def generarMatriz():
 	archivo = open("matrizDeTransicion.txt","w")	
 	cadena = ''
-	for m in range (0,cant+1):
-		for n in range (0,cant+1):
-			cadena = cadena + str(funcionProbabilidad(m,n)) + ','
+	matriz=[]
+	for m in range (0,cant):
+		fila=[]
+		for n in range (0,cant):
+			valor=funcionProbabilidad(m,n) 				
+			fila.append(valor)
+			cadena = cadena + str(valor) + ','
 		archivo.write(cadena + os.linesep)
+		matriz.append(fila)
 		cadena=''
-	archivo.close()		
+	archivo.close()	
+	return matriz	
 
 #a) genero la matriz de transición	
-generarMatriz()
+matrizDeTransicion = generarMatriz()
 	
 # La matriz de transición de estados se obtiene de la siguiente manera, dado que el ejercicio impone por simplicidad que la cantidad máxima
 # de clientes que se permite en simultaneo en el Homebanking es 50, entonces la matriz de transición es de 51 x 51 (Tambien se incluye la 
@@ -53,25 +60,59 @@ generarMatriz()
 
 #b) 
 
-#def generarSiguienteEstado(estadoActual):
-
- #   if (estadoActual <= 0):
-  #      j = numpy.arange(estadoActual, 51)  
-  #      estado = numpy.random.choice(j, p=matriz)  
-
-   # return estado
+def generarOtroEstado(Lista):
+	aleatorio = random.random()
+	sumaTotal = Lista[0]
+	i=0
+	while(aleatorio > sumaTotal or i<50 ):
+		if(i<50):	
+			sumaTotal = sumaTotal + Lista[i]
+			i=i+1
+	return i
 
 estados = []
 estadoActual = 0
+estados.append(estadoActual)
 observaciones = 100
 
-#for i in range(observaciones):  
- #   estadoActual = generarSiguienteEstado(estadoActual)
-  #  estados.append(estadoActual)
+#simulo los valores de los estados en cada momento
+for i in range(observaciones):  
+	estadoActual = generarOtroEstado(matrizDeTransicion[estadoActual])
+	estados.append(estadoActual)
 
-# valores de los estados en cada momento
-#plt.plot(estados)
-#plt.show()		
+#grafico de como se modifican los clientes a lo largo de las observaciones.
+plt.plot(estados)
+plt.title("Evolución del sistema")
+plt.xlabel('Observaciones')
+plt.ylabel('Conectados')
+plt.show()		
+
+#c)
+estados = []
+estadoActual = 0
+estados.append(estadoActual)
+observaciones = 100000
+
+#simulo los valores de los estados en cada momento
+for i in range(0,observaciones):
+	estadoActual = generarOtroEstado(matrizDeTransicion[estadoActual])	
+	estados.append(estadoActual)
+
+#Histograma
+plt.title('Histograma')
+plt.xlabel('Estados')
+plt.ylabel('Conectados')
+plt.hist(estados, bins =30, alpha=0.5, ec='black')
+plt.grid(True)
+plt.show()
+
+
+#d)
+
+
+
+
+
 
 #e)
 #P(i-->j >=40) <= 0.1
@@ -86,12 +127,5 @@ if (probabilidad < 0.1):
 else:
 	print("No es recomendable hacer la migración a otro servidor")	 
 				
-	
-
-
-
-
-
-	
 
 

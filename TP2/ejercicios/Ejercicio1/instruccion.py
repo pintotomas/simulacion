cf43629e96a8ada5b6a7c02b15f3e5d4dc6da555
Tomas <tomasp834@gmail.com>
2019-06-29 04:20:12 -0300

    arreglo ej1

--- TP2/ejercicios/Ejercicio1/instruccion.py
+++ TP2/ejercicios/Ejercicio1/instruccion.py
@@ -1,35 +1,55 @@
 import random
-SECOND_TO_MICROSECOND = 1000000
+import numpy
+
+PROBA_ACCEDER_MEMORIA = 0.65
+PROBA_SER_SIMPLE = 0.6
+PROBA_ACCEDER_CACHE = 0.6
+TASA_ACCESO_CACHE = 500
+TASA_ACCESO_MEMORIA_CON_CACHE = 1500
+TASA_ACCESO_MEMORIA_SIN_CACHE = 2000
+TASA_EJECUCION_INSTRUCCION_SIMPLE = 60
+TASA_EJECUCION_INSTRUCCION_COMPLEJA = 10 
+
 class InstruccionFactory(object):
   
   @staticmethod
   def nueva_instruccion():
-    # Tiro un random entre 0 y 1, si es menor o igual a 0.6 entonces
-    # es una instruccion simple 
-    # idem memoria
     tipo = random.uniform(0,1)
     memoria = random.uniform(0,1)
     lee_memoria = False
-    if (memoria <= 0.65):
+    if (memoria <= PROBA_ACCEDER_MEMORIA):
       lee_memoria = True
-    if (tipo <= 0.6):
+    if (tipo <= PROBA_SER_SIMPLE):
       return InstruccionSimple(lee_memoria)
     else:
       return InstruccionCompleja(lee_memoria)
 
-class InstruccionCompleja(object):
+class Instruccion(object):
+
+  def __init__(self, lee_memoria, tasa):
+    self.lee_memoria = lee_memoria
+    self.tasa_tiempo_ejecucion = tasa
+
+  def costo(self, modo):
+    costo = numpy.random.exponential((1/self.tasa_tiempo_ejecucion))
+    if self.lee_memoria and modo == "CACHE":
+      u = random.uniform(0,1)
+      if u <= PROBA_ACCEDER_CACHE:
+        costo += numpy.random.exponential((1/TASA_ACCESO_CACHE))
+      else:
+        costo += numpy.random.exponential((1/TASA_ACCESO_MEMORIA_CON_CACHE))
+    elif self.lee_memoria: #Alt 1
+      costo += numpy.random.exponential((1/TASA_ACCESO_MEMORIA_SIN_CACHE))
+    return costo
+
+class InstruccionCompleja(Instruccion):
 
   def __init__(self, lee_memoria):
-    self.lee_memoria = lee_memoria
+    super().__init__(lee_memoria, TASA_EJECUCION_INSTRUCCION_COMPLEJA)
 
-  def costo(self):
-    return 10/float(SECOND_TO_MICROSECOND)
-
-class InstruccionSimple(object):
+class InstruccionSimple(Instruccion):
 
   def __init__(self, lee_memoria):
-    self.lee_memoria = lee_memoria
+    super().__init__(lee_memoria, TASA_EJECUCION_INSTRUCCION_SIMPLE)
 
-  def costo(self):
-    return 60/float(SECOND_TO_MICROSECOND)
     
--- TP2/ejercicios/Ejercicio1/main.py
+++ TP2/ejercicios/Ejercicio1/main.py
@@ -1,14 +1,34 @@
-import OS
-import sys
-import threading
-opciones = ["alternativa_1", "alternativa_2"]
-def main():
-  if len(sys.argv) != 2:
-    print("Ingrese un argumento: alternativa_1 o alternativa_2")
-    return
-  elif sys.argv[1] not in opciones:
-    print("Argumento invalido. Usar alternativa_1 o alternativa_2")
-  else:
-    threading.Thread(target=OS.run, args=(sys.argv[1],)).start()
-main()
 
+TASA_ARRIBO_INSTRUC = 250
+n = 100000 #cantidad de instrucciones que genero
+
+iteraciones = 20
+
+def run(modo):
+  import instruccion
+  import numpy
+  if modo is not "CACHE" and modo is not "RAM":
+    print("MODO INCORRECTO")
+    return 
+
+  generador_instrucciones = instruccion.InstruccionFactory()
+  tiempo_proceso = float(0)
+  for x in range(n):
+    tiempo_de_espera_arribo_instruc = numpy.random.exponential((1/TASA_ARRIBO_INSTRUC))
+    instruccion = generador_instrucciones.nueva_instruccion()
+    tiempo_proceso += tiempo_de_espera_arribo_instruc + instruccion.costo(modo)
+  return tiempo_proceso
+
+print("Corriendo alternativa 1 20 veces")
+tiempo_total_alt1 = 0
+for x in range(iteraciones):
+  tiempo = run("RAM")
+  tiempo_total_alt1 += tiempo
+print("Tiempo promedio alternativa 1:" + str(round(tiempo_total_alt1/iteraciones, 2)))
+
+print("Corriendo alternativa 2 20 veces")
+tiempo_total_alt2 = 0
+for x in range(iteraciones):
+  tiempo = run("CACHE")
+  tiempo_total_alt2 += tiempo
+print("Tiempo promedio alternativa 2:" + str(round(tiempo_total_alt2/iteraciones, 2)))
